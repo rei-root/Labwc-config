0a0bfa11e28c9fe3714a49471a949da32061145d
rei-root <seytumerov_rusya@mail.ru>
2026-06-20 22:17:07 +0300

    Add wallpaper change via swww

---
+++ labwc/scripts/wallpaper-select.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+WALL_DIR="$HOME/Pictures/Wallpapers"
+
+if ! pgrep -x "awww-daemon" > /dev/null; then
+    awww-daemon &
+    sleep 0.1
+fi
+
+SELECTION=$(ls "$WALL_DIR" | fuzzel -d -p "Select wallpaper > ")
+
+if [ -z "$SELECTION" ]; then
+    exit 0
+fi
+
+FINAL_WALL="$WALL_DIR/$SELECTION"
+swww img "$FINAL_WALL" --transition-type wipe --transition-step 90 --transition-fps 60
